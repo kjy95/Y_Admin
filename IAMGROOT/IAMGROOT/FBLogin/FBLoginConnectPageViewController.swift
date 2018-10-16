9ddadc97bc0ae33719ed0563c0e606a96c3efd04
김지영 <rmarkd32145@naver.com>
2018-10-16 12:15:04 +0900

    create groupfolder 'FBLogin' , file 'FBLoginConnectPageViewController.xib' & 'FBLoginConnectPageViewController.swift' It would connect firebase login

---
+++ IAMGROOT/IAMGROOT/FBLogin/FBLoginConnectPageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FBLoginConnectPageViewController.swift
+//  IAMGROOT
+//
+//  Created by 김지영 on 2018. 10. 16..
+//  Copyright © 2018년 김지영. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FBLoginConnectPageViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
