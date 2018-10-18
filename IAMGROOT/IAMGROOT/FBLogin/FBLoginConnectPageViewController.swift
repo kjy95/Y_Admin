acb91e11cfb7fe98cd0c5580162c5ed832fff8b3
김지영 <rmarkd32145@naver.com>
2018-10-18 21:40:23 +0900

    create firebase UI(google login) in main_story_board. It can login google id and pw now. I should move it to FBLoginConnectPageViewController.

--- IAMGROOT/IAMGROOT/FBLogin/FBLoginConnectPageViewController.swift
+++ IAMGROOT/IAMGROOT/FBLogin/FBLoginConnectPageViewController.swift
@@ -10,18 +10,18 @@ import Foundation
 import UIKit
 import Firebase
 import GoogleSignIn
+import FirebaseUI
 
-class FBLoginConnectPageViewController : UIViewController {
-    
+class FBLoginConnectPageViewController : UIViewController{ 
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
 }
--- IAMGROOT/IAMGROOT/ViewController.swift
+++ IAMGROOT/IAMGROOT/ViewController.swift
@@ -6,27 +6,33 @@
 //  Copyright © 2018년 김지영. All rights reserved.
 //
 
+
+import Foundation
 import UIKit
+import Firebase
+import GoogleSignIn
+import FirebaseUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInUIDelegate {
+    @IBOutlet weak var signInButton: GIDSignInButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        presentMainViewController()
-        // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func presentMainViewController() {
-        let mainVC = ViewController(nibName:"FBLoginConnectPageViewController", bundle:nil)
-        self.navigationController?.pushViewController(mainVC, animated: true);
-        // you could present it another way, such as:
-        // self.presentViewController(mainVC, true, nil)
-        // to present it as a modal
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
     }
-
-
 }
