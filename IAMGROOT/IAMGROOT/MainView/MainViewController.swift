8ec598d082b700d21e0cac0ce6daf6972cf7187f
김지영 <rmarkd32145@naver.com>
2018-10-25 20:12:27 +0900

    when update data, the label text changed textfield's text.

--- IAMGROOT/IAMGROOT/MainView/MainViewController.swift
+++ IAMGROOT/IAMGROOT/MainView/MainViewController.swift
@@ -16,13 +16,14 @@ import FirebaseUI
 class MainViewController : UIViewController {
     @IBOutlet weak var textfield1: UITextField!
     @IBOutlet weak var textfield2: UITextField!
+    @IBOutlet weak var label1: UILabel!
     
     var ref: DatabaseReference!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Get a secondary database instance by URL
         ref = Database.database(url: "https://atticyadmin-10a61.firebaseio.com/").reference()
-    
+        getInfoFromFBRDB()
     }
     
     @IBAction func buttonAction(_ sender: Any) {
@@ -30,6 +31,18 @@ class MainViewController : UIViewController {
         let location: String = textfield1.text!
         
         self.ref.child("users").child(User.uid).setValue(["location": location])
+        getInfoFromFBRDB()
+    }
+    func getInfoFromFBRDB(){
+        let userID = Auth.auth().currentUser?.uid
+        ref.child("users").child(User.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            print(value)
+            self.label1.text = value!["location"] as? String
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
