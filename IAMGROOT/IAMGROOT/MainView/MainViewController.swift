6687fc96b97f633a73bb95057b1a43be2054565a
김지영 <rmarkd32145@naver.com>
2018-10-27 22:09:45 +0900

    when push button, the x,y location will be updated on db. and the google map show the view of the location.

--- IAMGROOT/IAMGROOT/MainView/MainViewController.swift
+++ IAMGROOT/IAMGROOT/MainView/MainViewController.swift
@@ -18,6 +18,9 @@ class MainViewController : UIViewController {
     @IBOutlet weak var textfield1: UITextField!
     @IBOutlet weak var textfield2: UITextField!
     @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var textfield3: UITextField!
     
     var ref: DatabaseReference!
     //Take a Google Map Object. Don't make outlet from Storyboard, Break the outlet of GMSMapView if you made an outlet
@@ -27,33 +30,46 @@ class MainViewController : UIViewController {
         super.viewDidLoad()
         // Get a secondary database instance by URL
         ref = Database.database(url: "https://atticyadmin-10a61.firebaseio.com/").reference()
-        getInfoFromFBRDB()
+        //changeLabel_getInfoFromFBRDB()
+        
+    }
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        //
+        let withLatitude: String = textfield1.text!
+        let longitude: String = textfield3.text!
+        let lock: String = textfield2.text!
+        
+        self.ref.child("users").child(User.uid).setValue(["withLatitude": withLatitude,"longitude": longitude,"lock": lock])
+        changeLabel_getInfoFromFBRDB()
+        showgooglemap(x:withLatitude, y: longitude)
+    }
+    func changeLabel_getInfoFromFBRDB(){
+        ref.child("users").child(User.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            self.label1.text = value!["withLatitude"] as? String
+            self.label2.text = value!["longitude"] as? String
+            self.label3.text = value!["lock"] as? String
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+    func showgooglemap(x:String, y: String){
         //google map
-        mapView = GMSMapView.map(withFrame: CGRect(x: 59, y: 280, width: 350, height: 350), camera: GMSCameraPosition.camera(withLatitude: 51.050657, longitude: 10.649514, zoom: 5.5))
+        //string to float
+        let numberFormatter = NumberFormatter()
+        let numberx = numberFormatter.number(from: x)
+        let numberFloatValuex = numberx?.floatValue
+        let numbery = numberFormatter.number(from: y)
+        let numberFloatValuey = numbery?.floatValue
+
+        mapView = GMSMapView.map(withFrame: CGRect(x: 39, y: 290, width: 350, height: 350), camera: GMSCameraPosition.camera(withLatitude: CLLocationDegrees(numberFloatValuex!), longitude: CLLocationDegrees(numberFloatValuey!), zoom: 6.5))
         
         //so the mapView is of width 200, height 200 and its center is same as center of the self.view
         //mapView?.center = self.view.center
         
         self.view.addSubview(mapView!)
-    }
-    
-    @IBAction func buttonAction(_ sender: Any) {
-        //
-        let location: String = textfield1.text!
-        
-        self.ref.child("users").child(User.uid).setValue(["location": location])
-        getInfoFromFBRDB()
-    }
-    func getInfoFromFBRDB(){
-        let userID = Auth.auth().currentUser?.uid
-        ref.child("users").child(User.uid).observeSingleEvent(of: .value, with: { (snapshot) in
-            // Get user value
-            let value = snapshot.value as? NSDictionary
-            print(value)
-            self.label1.text = value!["location"] as? String
-        }) { (error) in
-            print(error.localizedDescription)
-        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
