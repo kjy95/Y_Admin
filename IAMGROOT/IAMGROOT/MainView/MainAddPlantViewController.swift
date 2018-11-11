dd0562deeb295318598f152a686e2fd6d5826375
김지영 <rmarkd32145@naver.com>
2018-11-11 19:17:45 +0900

    add mainaddplantviewcontroller to add plant. it contain search textfield.

---
+++ IAMGROOT/IAMGROOT/MainView/MainAddPlantViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainAddPlantViewController.swift
+//  IAMGROOT
+//
+//  Created by 김지영 on 11/11/2018.
+//  Copyright © 2018 김지영. All rights reserved.
+//
+
+import UIKit
+
+class MainAddPlantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
